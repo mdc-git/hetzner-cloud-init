9cd71c3f7282c73b966a2b0106deadd02b0457fd
mdc-git <65224188+mdc-git@users.noreply.github.com>
2021-03-06 12:55:57 +0100

    Update update-config.sh

--- update-config.sh
+++ update-config.sh
@@ -25,33 +25,6 @@ case $key in
 esac
 done
 
-NEW_NODE_IPS=( $(curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" 'https://api.hetzner.cloud/v1/servers' | jq -r '.servers[].public_net.ipv4.ip') )
-
-touch /etc/current_node_ips
-cp /etc/current_node_ips /etc/old_node_ips
-echo "" > /etc/current_node_ips
-
-for IP in "${NEW_NODE_IPS[@]}"; do
-  ufw allow from "$IP"
-  echo "$IP" >> /etc/current_node_ips
-done
-
-IFS=$'\r\n' GLOBIGNORE='*' command eval 'OLD_NODE_IPS=($(cat /etc/old_node_ips))'
-
-REMOVED=()
-for i in "${OLD_NODE_IPS[@]}"; do
-  skip=
-  for j in "${NEW_NODE_IPS[@]}"; do
-    [[ $i == $j ]] && { skip=1; break; }
-  done
-  [[ -n $skip ]] || REMOVED+=("$i")
-done
-declare -p REMOVED
-
-for IP in "${REMOVED[@]}"; do
-  ufw delete allow from "$IP"
-done
-
 FLOATING_IPS=${FLOATING_IPS:-"0"}
 
 if [ "$FLOATING_IPS" == "1" ]; then
